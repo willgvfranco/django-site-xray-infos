c2cd24a5231938d9198670e0800ca295c3deb922
neus <williamgvfranco@gmail.com>
2021-01-04 22:40:35 -0300

    prod neus

--- xray/settings.py
+++ xray/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['xray.neustren.com.br/', '127.0.0.1']
 
 
 # Application definition
